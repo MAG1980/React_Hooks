1e9c2c486100d8eedd7da48aab1ee7991b13aa17
MAG1980 <al-1980@inbox.ru>
2023-04-08 18:00:52 +0300

    Создана и применена функция generateDummies для демонстрации асинхронной работы useEffect.

--- src/components/MovingDiv.jsx
+++ src/components/MovingDiv.jsx
@@ -1,19 +1,47 @@
 import {usePosition} from "../hooks/usePosition";
+import {useState, useEffect} from "react";
 
+const initialStyle = {
+    backgroundColor: '#F00',
+    position: 'absolute',
+    width: 100,
+    height: 100,
+    left: 0,
+    top: 0
+}
+
+const generateDummies = (count) => {
+    const dummies = []
+    for (let i = 0; i < count; i++) {
+        dummies.push(<div>i = {i}</div>)
+    }
+    return dummies
+}
 export const MovingDiv = () => {
     const [left, top] = usePosition(50)
+    const [style, setStyle] = useState(initialStyle);
+
+    useEffect(() => {
+        setStyle((prevState) => {
+            return {
+                ...prevState,
+                left,
+                top
+            }
+        }
+    )
+        return () => {
+
+        };
+    }, [left, top]);
+
+
     return (
         <>
             <h2>[{left}, {top}]</h2>
-            <div style={{
-                backgroundColor: '#F00',
-                position: 'absolute',
-                width: 100,
-                height: 100,
-                left,
-                top
-            }}>
+            <div style={style}>
             </div>
+            {generateDummies(30000)}
         </>
     )
 }
