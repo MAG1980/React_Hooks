aefb5eef29042f80041996fa121868a6efe7bad8
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-04-19 17:17:24 +0300

    Ререндеринг родительского компонента перестал вызывать ререндер дочерних компонентов благодаря тому, что кроме мемоизации ссылок на фунции-обработчики, передаваемые через props с помощью useCallback, дочерние компоненты обёрнуты в React.memo.

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,12 +1,23 @@
-export const ListItem = ({item, onClick}) => {
-    const {name}=item
-    console.log(`> render ${name}`)
+import React from "react";
+/*эквивалентный импорт
+import {memo} from "react";*/
 
-    return (
-        //В данном случае использовать useCallback нет необходимости,
-        // т.к. callback используется именно в этом компоненте.
-        <div onClick={()=>onClick(item)}>
-            {name}
-        </div>
-    )
-}
+
+//Дочерний компонент ListItem обёрнут в React.memo,
+//что позволяет избежать лишних рендеров при неизменных props
+//при ререндеринге родительского компонента.
+/*export const ListItem = memo(*/
+export const ListItem = React.memo(
+    ({item, onClick}) => {
+        const {name} = item
+        console.log(`> render ${name}`)
+
+        return (
+            //В данном случае использовать useCallback нет необходимости,
+            // т.к. callback используется именно в этом компоненте.
+            <div onClick={() => onClick(item)}>
+                {name}
+            </div>
+        )
+    }
+)
