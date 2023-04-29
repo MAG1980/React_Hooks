5b7db88b1a5014032516de68b002a6d62d8af65f
MAG1980 <al-1980@inbox.ru>
2023-04-29 10:03:44 +0300

    Создан хук useLocalStorage.

---
+++ src/components/CustomHooks/useLocalStorage/useLocalStorage.js
@@ -0,0 +1,60 @@
+import {useState} from "react";
+
+/**
+ * Проверка аргумента на принадлежность к типу function
+ * @param valueOrFunction
+ * @return {boolean}
+ */
+function isFunction(valueOrFunction) {
+    return typeof valueOrFunction === 'function'
+}
+
+/**
+ * Предоставляет удобный API для работы с localStorage.
+ * Вызывает ререндер компонента при обновлении данных в localStorage.
+ * @param key
+ * @param initialValue
+ * @return {unknown[]}
+ */
+export function useLocalStorage(key, initialValue) {
+    //Принимает callback в качестве начального значения для ленивой инициализации
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            //Попытка получить из localStorage ранее сохранённого значения
+            const item = window.localStorage.getItem(key)
+            if (item) {
+                //По умолчанию в localStorage все данные, в том числе и объекты, хранятся в string
+                return JSON.parse(item)
+            }
+
+            //Если ранее сохранённое значение в localStorage отсутствует,
+            //то рассчитывает начальное значение state в зависимости от того, является ли initialValue функцией
+            const evaluated = isFunction(initialValue)
+                ? initialValue()
+                : initialValue
+
+            //Сохраняет в localStorage преобразованное в строку вычисленное начальное значение по переданному ключу
+            window.localStorage.setItem(key, JSON.stringify(evaluated))
+            return evaluated
+        } catch (error) {
+            console.error(error)
+            return initialValue
+        }
+    });
+
+    const setValue = (newValue) => {
+        try {
+            //Если в качестве нового значения передана функция,
+            //то вызываем её с текущим значением, хранящимся в state, переданным в качестве аргумента
+            const evaluated = isFunction(newValue) ? newValue(storedValue) : newValue
+            window.localStorage.setItem(key, JSON.stringify(evaluated))
+
+            //Вызываем рендер компонента, получающие данные, возвращаемые хуком
+            setStoredValue(evaluated)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return [storedValue, setStoredValue]
+}
