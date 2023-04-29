6dfd620f18a275aa187778d22f11c274a3ee68da
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-04-29 21:53:46 +0300

    Исправлена ошибка в хуке useAnimatedText. Добавлены комментарии.

--- src/components/CustomHooks/useAsync_&_useAnimatedText/useAnimatedText.js
+++ src/components/CustomHooks/useAsync_&_useAnimatedText/useAnimatedText.js
@@ -1,12 +1,24 @@
 import {useEffect, useState} from "react";
 
+/**
+ * Через равные интервалы времени возвращает подстстроку увеличивающейся длины, полученную из исходной
+ * @param text исходная строка текста
+ * @param delayMs интервал отображения символов
+ * @returns {string} подстрока
+ */
 export function useAnimatedText(text, delayMs) {
+    //Позиция последнего символа отображаемой подстроки.
     const [currentPos, setCurrentPos] = useState(0);
 
     useEffect(() => {
+        //
         const intervalHandle = setInterval(() => {
-            setCurrentPos(() => {
+            setCurrentPos((pos) => {
+                //Проверка текущей позиции на соответствие последнему символу строки
                 const isLast = pos === text.length - 1
+
+                //Если символ не последний, увеличиваем длину отображаемой строки на 1
+                //По достижении конца строки номер последнего отображаемого символа сбрасывается на 0
                 return isLast ? 0 : pos + 1
             })
         }, delayMs)
@@ -14,5 +26,7 @@ export function useAnimatedText(text, delayMs) {
             clearInterval(intervalHandle)
         };
     }, [text, delayMs]);
+
+    //Возвращаем строку, образанную до текущей позиции
     return text.substring(0, currentPos)
 }
